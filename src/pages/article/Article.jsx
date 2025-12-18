9711f09b8796eee36eb720d06169d4a4b2c71869
Han <gkscodud0527@gmail.com>
2025-12-18 20:37:11 +0900

    Feat: 아티클 페이지 디자인 수정

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -2,6 +2,7 @@ import { Container, Grid, Card, CardMedia, CardContent, Typography } from '@mui/
 import { useNavigate } from 'react-router-dom';
 
 import NavbarComponent from '../../components/Navbar'
+import MainContainer from '../../components/MainContainer';
 import { PATH } from "../../config/paths";
 import Footer from '../../components/Footer';
 
@@ -29,34 +30,44 @@ const ReportMain = () => {
     >
       <NavbarComponent />
 
-      <Container maxWidth="lg" style={{ marginTop: "120px", marginBottom: "60px" }}>
-        <Typography variant="h4" fontWeight="bold" gutterBottom>
-          아티클 리스트
+      <MainContainer>
+        <Typography
+          variant="h1"
+          fontWeight={700}
+          fontSize="28px"
+          marginBottom="16px"
+        >
+          자료 게시판
         </Typography>
 
-        <Grid container spacing={4}>
+        <Grid container spacing={2}>
           {articleList.map((article) => (
-            <Grid item xs={12} sm={6} md={4} key={article.id}>
-              <Card 
+            <Grid item xs={12} sm={6} md={3} key={article.id}>
+              <Card variant="outlined"
                 onClick={() => handleCardClick(article.id)}
                 sx={{ 
                   cursor: "pointer",
-                  borderRadius: 3,
-                  boxShadow: 3,
-                  "&:hover": { boxShadow: 6, transform: "translateY(-4px)", transition: "0.2s" }
+                  borderRadius: 2,
+                  padding: 2,
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  "&:hover": { transform: "translateY(-4px)", transition: "0.2s", border: "solid 1px var(--color-blue-500)" }
                 }}
               >
                 <CardMedia
                   component="img"
-                  height="180"
+                  height="200"
                   image={article.thumbnail}
                   alt={article.title}
+                  sx={{ borderRadius: 1, mb: 2 }}
                 />
-                <CardContent>
-                  <Typography variant="h6" fontWeight="bold">
+                <CardContent sx={{ padding: 0, "&:last-child": { pb: 0 } }}>
+                  <Typography variant="h6" fontWeight={700} fontSize="20px" lineHeight="30px"
+                    sx={{ mb: "4px" }}>
                     {article.title}
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
+                  <Typography variant="body2" fontWeight={400} fontSize="16px" lineHeight="24px" color="var(--color-gray-500)">
                     {article.summary}
                   </Typography>
                 </CardContent>
@@ -64,7 +75,15 @@ const ReportMain = () => {
             </Grid>
           ))}
         </Grid>
-      </Container>
+      </MainContainer>
+
+      {/* <Container maxWidth="lg" style={{ marginTop: "120px", marginBottom: "60px" }}>
+        <Typography variant="h4" fontWeight="bold" gutterBottom>
+          아티클 리스트
+        </Typography>
+
+
+      </Container> */}
 
       <Footer />
     </Container>
